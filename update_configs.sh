3eb65c77303193621eea5e6f8d4a9d67cd978623
KevinDev64 <kevindev56@yandex.ru>
2026-03-28 01:48:10 +0300

    feat: script only for replacing configs

---
+++ update_configs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+echo "Deleting old sites and config..."
+rm -v /etc/nginx/sites-available/* /etc/nginx/nginx.conf /etc/nginx/sites-enabled/*
+
+echo "Copying latest sites..."
+cp -v nginx/sites/* /etc/nginx/sites-available/.
+
+echo "Enabling sites..."
+ln -sv /etc/nginx/sites-available/* /etc/nginx/sites-enabled/.
+
+echo "Copying the latest nginx.conf..."
+cp -v nginx/nginx.conf /etc/nginx/.
+
+echo "Checking syntax and restarting nginx if all're ok..."
+nginx -t && systemctl restart nginx
